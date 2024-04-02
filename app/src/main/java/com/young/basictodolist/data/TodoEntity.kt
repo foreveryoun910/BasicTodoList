c27a0b6b3155e5fe510715713efec6b376dfe2a8
foreveryoun910 <foreveryoun910@gmail.com>
2024-04-02 16:49:27 +0900

    item_todo 구현

--- app/src/main/java/com/young/basictodolist/data/TodoEntity.kt
+++ app/src/main/java/com/young/basictodolist/data/TodoEntity.kt
@@ -7,9 +7,9 @@ import androidx.room.PrimaryKey
 @Entity
 data class TodoEntity (
     @PrimaryKey(autoGenerate = true) var id: Int? = null,
-    @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "task") var task: String,
+    @ColumnInfo(name = "memo") var memo: String,
     @ColumnInfo(name = "importance") var importance: Int,
-    @ColumnInfo(name = "description") var description: String,
     @ColumnInfo(name = "category") var category: String,
     @ColumnInfo(name = "date") var date: Long,
     @ColumnInfo(name = "time") var time: Long,
