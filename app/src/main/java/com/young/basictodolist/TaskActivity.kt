6cdc638f59c5fd8d2b810c362b4870a125d7f384
foreveryoun910 <foreveryoun910@gmail.com>
2024-04-12 14:59:56 +0900

    TaskActivity 작업

--- app/src/main/java/com/young/basictodolist/TaskActivity.kt
+++ app/src/main/java/com/young/basictodolist/TaskActivity.kt
@@ -1,11 +1,56 @@
 package com.young.basictodolist
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import com.young.basictodolist.data.AppDatabase
+import com.young.basictodolist.databinding.ActivityTaskBinding
+import java.util.Calendar
 
-class TaskActivity : AppCompatActivity() {
+const val DB_NAME = "todo.db"
+
+class TaskActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var myCalendar: Calendar
+    lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+    lateinit var timeSetListener: TimePickerDialog.OnTimeSetListener
+    var finalDate = 0L
+    var finalTime = 0L
+
+    private val labels = arrayListOf("Personal", "Business", "Insurance", "Shopping", "Banking")
+
+    val db by lazy {
+        AppDatabase.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
+    }
+
+    private fun setUpSpinner() {
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, labels)
+        labels.sort()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.edt_date -> {
+                //
+            }
+            R.id.edt_time -> {
+                //
+            }
+            R.id.btn_add_task -> {
+                addTask()
+            }
+        }
+    }
+
+    private fun addTask() {
+        //
     }
 }
