b2384ef33e6a286b8f6c80a89529871e8a9db659
foreveryoun910 <foreveryoun910@gmail.com>
2024-05-02 16:19:10 +0900

    TodoAdapter 작업중

--- app/src/main/java/com/young/basictodolist/MainActivity.kt
+++ app/src/main/java/com/young/basictodolist/MainActivity.kt
@@ -2,11 +2,16 @@ package com.young.basictodolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.young.basictodolist.adapter.TodoAdapter
+import com.young.basictodolist.data.TodoEntity
 import com.young.basictodolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
+
+    var list = arrayListOf<TodoEntity>()
+    var adapter = TodoAdapter(list)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/young/basictodolist/adapter/TodoAdapter.kt
+++ app/src/main/java/com/young/basictodolist/adapter/TodoAdapter.kt
@@ -1,4 +1,41 @@
 package com.young.basictodolist.adapter
 
-class TodoAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.young.basictodolist.R
+import com.young.basictodolist.data.TodoEntity
+import java.util.Random
+
+class TodoAdapter(val list: List<TodoEntity>) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun getItemId(position: Int): Long {
+        return list[position].id?.toLong() ?: 0L
+    }
+
+    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(todoEntity: TodoEntity) {
+            with(itemView) {
+                val colors = resources.getIntArray(R.array.random_color)
+                val randomColor = colors[(Random().nextInt(colors.size))]
+                //viewColorTag.setBackgroundColor(randomColor)
+            }
+        }
+    }
+
 }
